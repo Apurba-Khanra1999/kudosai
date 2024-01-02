cbefd48e88cd9c17d933ecccabf786e3c24de20c
Apurba Khanra <77966841+Apurba-Khanra1999@users.noreply.github.com>
2024-01-02 16:02:16 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -35,14 +35,23 @@ import PIL.Image
 import streamlit as st
 import requests
 from io import BytesIO
+
+st.set_page_config(
+    page_title="Kudos AI",
+    page_icon="🤖",
+    layout="wide",
+)
+
 genai.configure(api_key="AIzaSyCyGBa2I6Y_fsqXXpdk-JYK3Z5TPMM4Bgo")
 model = genai.GenerativeModel("gemini-pro-vision")
 
-uploaded_file = st.file_uploader("Choose an image", type=["jpg", "jpeg", "png", "bmp"])
-
-
-
-uploaded_text = st.text_input('What to do with the image?')
+st.header("Describe the Image")
+filecol, textcol = st.columns(2)
+with filecol:
+    uploaded_file = st.file_uploader("Choose an image", type=["jpg", "jpeg", "png", "bmp"])
+with textcol:
+    st.text('Do more with the Image \neg. Write a blog based on the image...\n    What is the recipe of the food? etc.')
+    uploaded_text = st.text_input('What to do with the image?')
 
 if uploaded_file is not None:
     img = PIL.Image.open(uploaded_file)
@@ -54,12 +63,19 @@ if uploaded_file is not None:
         st.image(img, use_column_width=True)
 
     # Generate content based on image
-    if st.button("Describe Image"):
-        with st.spinner("Generating content..."):
-            response = model.generate_content([uploaded_text,img], stream=True)
-            response.resolve()
-        # Display generated text in right column
-        with col2:
-            st.write(response.text)
-else:
-    st.write("Please upload an image to get the detailed description of the image")
+    with col2:
+        if st.button("Describe Image"):
+            with st.spinner("Generating content..."):
+                response = model.generate_content([uploaded_text, img], stream=True)
+                response.resolve()
+                st.write(response.text)
+
+
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
